549ddb1236540af118f097399880c27208c7d635
Meenu <meenaranjinik@gmail.com>
2025-11-12 11:01:59 +0530

    correction

--- react/myapp/src/Meenu.js
+++ react/myapp/src/Meenu.js
@@ -1,14 +1,13 @@
 
-//object as Parameter
+//----object as Parameter
 // function Meenu({name}) {
 //   return (
-
 //             <h2> My Name is {name} </h2>
 //   );
 // }
 // export default Meenu ;
 
-//arrow function
+//----------arrow function
 // let Meenu=({name})=> {
 //   return (
 //             <h2> My Name is {name} </h2>
@@ -16,22 +15,20 @@
 // }
 // export default Meenu ;
 
-// anomynaus function---
-// let Meenu=function({name}) {
-//   return (
-//             <h2> My Name is {name} </h2>
-//   );
-// }
-// export default Meenu ;
-
+// --------anomynaus function---
+let Myname=function({name}) {
+  return (
+            <h2> My Name is {name} </h2>
+  );
+}
+export default Myname ;
 
 function Meenu(props) {
   return (
     <>
-        <b>First Name</b><i>{props.fname}</i>
-        <b>Last Name</b><i>{props.lname}</i>
+      <b>First Name</b><i>{props.fname}</i>
+      <b>Last Name</b><i>{props.lname}</i>
     </>
-       
   );
 }
-export default Meenu;
+export {Meenu};
--- react/myapp/src/index.js
+++ react/myapp/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import Meenu from './Meenu';
+import {Meenu} from './Meenu';
+import Myname from './Meenu';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  {/* <Meenu  name="Meenu"/> */}
+  <Myname name="Meenu"/>
   <Meenu fname="Meera" lname="Ranju"/>
   </React.StrictMode>
 );
