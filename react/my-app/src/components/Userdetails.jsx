f4582de7635f489a24bbfd1928e3fa32ef7a0c26
Meenu <meenaranjinik@gmail.com>
2026-01-01 11:12:21 +0530

    Enhance user details UI and update styles
    Improved the UserDetails component layout by wrapping content in a styled card and updating button classes. Expanded App.css with new styles for cards, buttons, containers, and headings to enhance the overall UI appearance.

--- react/my-app/src/components/Userdetails.jsx
+++ react/my-app/src/components/Userdetails.jsx
@@ -1,35 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 function UserDetails() {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const [user, setUser] = useState(null);
-  const [totalusers, setTotaluser] = useState();
-  useEffect(() => {
-    fetch(`https://dummyjson.com/users/${id}`)
-      .then(res => res.json())
-      .then(data => setUser(data));
-  }, [id]);
-      const handlerdelete = (id) => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [totalusers, setTotaluser] = useState();
+    useEffect(() => {
+        fetch(`https://dummyjson.com/users/${id}`)
+            .then(res => res.json())
+            .then(data => setUser(data));
+    }, [id]);
+    const handlerdelete = (id) => {
         const filterUser = user.filter((user) => {
             return (user.id !== id);
         });
         setUser(filterUser);
         setTotaluser(filterUser.length);
     }
-  if (!user) return <p>Loading...</p>;
-  return (
-    <div>
-      <h2>User Details</h2>
-      <img src={user.image} width="120" alt="user"/>
-      <p><b>Name:</b> {user.firstName} {user.lastName}</p>
-      <p><b>Email:</b> {user.email}</p>
-      <p><b>Phone:</b> {user.phone}</p>
-      <button onClick={() => {
-                            handlerdelete(user.id)
-                        }}>Delete</button><br/>
-      <button onClick={() => navigate(-1)}>← Back</button>
-    </div>
-  );
+    if (!user) return <p>Loading...</p>;
+    return (
+        <div>
+             <div className='card-student'>
+                <h2>User Details</h2>
+                <img src={user.image} width="120" alt="user" />
+                <p><b>Name:</b> {user.firstName} {user.lastName}</p>
+                <p><b>Email:</b> {user.email}</p>
+                <p><b>Phone:</b> {user.phone}</p>
+                <button  onClick={() => {
+                    handlerdelete(user.id)
+                }}>Delete</button><br />
+                <button class="back" onClick={() => navigate(-1)}>← Back</button>
+            </div>
+
+        </div>
+    );
 }
 export default UserDetails;
